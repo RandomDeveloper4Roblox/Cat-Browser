8b812852c938c9f79292c3747b274ba558d53036
RandomDeveloper4Roblox <108463732+RandomDeveloper4Roblox@users.noreply.github.com>
2022-07-04 18:02:36 +0100

    Project stuff

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Cat Browser")>
+<Assembly: AssemblyDescription("Browse the inernet")>
+<Assembly: AssemblyCompany("Jess the Cat")>
+<Assembly: AssemblyProduct("Cat Browser")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("jessacat")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("f6a3ab39-3895-4482-a588-5bf80e46820a")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("2.0.0.0")>
+<Assembly: AssemblyFileVersion("2.0.0.0")>
